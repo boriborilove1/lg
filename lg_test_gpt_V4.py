26b3c1a79d800e03e825fc02f2479a19a8eedf5a
boriborilove1 <jeongjuni2025@gmail.com>
2025-03-08 17:42:22 +0900

    Update lg_test_gpt_V4.py

--- lg_test_gpt_V4.py
+++ lg_test_gpt_V4.py
@@ -44,7 +44,7 @@ url1 = 'https://www.lguplus.com/login'
 pay_url = 'https://www.lguplus.com/mypage/payinfo?p=1'
 
 XPATHS = {
-    "kakao_login_btn": '//img[@src='https://www.lguplus.com/static/pc-static/nmem/images/icon_kakao.png' and @alt='카카오']',
+    "kakao_login_btn": "//img[@src='https://www.lguplus.com/static/pc-static/nmem/images/icon_kakao.png' and @alt='카카오']",
     "login_btn": '/html/body/div/div/div/main/article/div/div/form/div[4]/button[1]',
     "pay_btn": '/html/body/div[1]/div/div/main/div[1]/div/div[2]/div/div/div/div[2]/div[1]/div/div[3]/button[1]',
     "confirm_pay_btn1": '/html/body/div[8]/div[1]/div/div/footer/button[2]',
