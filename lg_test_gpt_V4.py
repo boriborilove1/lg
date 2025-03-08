6689770534d8f4639266a15476f9e4d048f79feb
boriborilove1 <jeongjuni2025@gmail.com>
2025-03-08 17:19:02 +0900

    Update lg_test_gpt_v5.py
    버튼 로딩 추가

--- lg_test_gpt_V4.py
+++ lg_test_gpt_V4.py
@@ -62,7 +62,8 @@ try:
     # driver.find_element(By.CSS_SELECTOR, XPATHS["kakao_login_btn"]).click()
     # time.sleep(2)
     #버튼 로딩될때까지 대기(10초)
-    kakao_login_btn = WebDriverWait(driver, 10).until(
+    print("로그인 버튼 로딩될때까지 대기(5초)")
+    kakao_login_btn = WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, XPATHS["kakao_login_btn"]))
     )
 
