83f927033d26ea67d033cb1543b0ddd83d09c95e
boriborilove1 <jeongjuni2025@gmail.com>
2025-03-10 12:10:47 +0900

    Update lg_test_gpt_v5.py

--- lg_test_gpt_v5.py
+++ lg_test_gpt_v5.py
@@ -74,8 +74,7 @@ try:
     print(f"[✅] 카카오 로그인 버튼을 클릭하기 위해 로딩될 때까지 10초 대기합니다.")
     # pay_button = WebDriverWait(driver, 10).until(
     #     EC.element_to_be_clickable((By.XPATH, XPATHS["kakao_login_btn"]))) #버튼이 보일 때까지 10초 대기
-    pay_button = driver.find_element(By.XPATH, XPATHS["kakao_login_btn"])
-    pay_button.click()
+    driver.find_element(By.XPATH, XPATHS["kakao_login_btn"]).click()
     print(f"[✅] 카카오 로그인 버튼 클릭완료")
     time.sleep(2)
     # driver.find_element(By.XPATH, XPATHS["kakao_login_btn"]).click()
